57db5be1abbfdcb312bf72b1294682e146fb8198
Wayne Boka <boka.wayne@gmail.com>
2015-11-14 19:33:15 -0500

    Adds code to display new HTML5 elements correctly in old browsers. Fixes display issue with .clear-fix. Fixes bug with the menu toggle on mobile. Click events on "register" on a and input elements. Adds clear fix to adjust for floated divs. Adds ability to display the menu correctly when resizing the screen.

--- js/SimplyResponsive.js
+++ js/SimplyResponsive.js
@@ -1,4 +1,10 @@
 var SimplyResponsive = {
+	breakpoints: {
+		sm: 480,
+		md: 768,
+		lg: 992,
+		xl: 1200
+	},
 	toggleClass: function (elements, className) {
 		for (var i = 0; i < elements.length; i++) {
 			elements[i].classList.toggle(className);
@@ -9,13 +15,28 @@ var SimplyResponsive = {
 
 		for (var i = 0; i < menuToggles.length; i++) {
 			menuToggles[i].addEventListener("click", function() {
-				menusToToggle = this.parentNode.getElementsByClassName("menu");
+				menusToToggle = this.parentNode.parentNode.parentNode.getElementsByClassName("menu");
 
 				SimplyResponsive.toggleClass(menusToToggle, "display");
 				SimplyResponsive.toggleClass(menusToToggle, "display-sm");
-				SimplyResponsive.toggleClass(menusToToggle.getElementsByTagName("ul"), "list-inline");
+				
+				for (var j = 0; j < menusToToggle.length; j++) {
+					SimplyResponsive.toggleClass(menusToToggle[j].getElementsByTagName("ul"), "list-inline");
+				}
 			});
 		}
+		
+		window.addEventListener("resize", function() {
+			menusToToggle = document.querySelectorAll(".menu ul");
+			
+			for (var i = 0; i < menusToToggle.length; i++) {
+				if (window.innerWidth >= SimplyResponsive.breakpoints.sm) {
+					menusToToggle[i].classList.add("list-inline");
+				} else {
+					menusToToggle[i].classList.remove("list-inline");
+				}
+			}
+		});
 	}
 },
 SR = SimplyResponsive;
